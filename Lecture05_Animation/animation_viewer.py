9e62e8f9eff81825d6bb647d5ddd1520e4886112
Cho Kisung <kisung128@gmail.com>
2025-09-22 12:28:47 +0900

    5종 애니메이션을 무한루프하는 토대

--- Lecture05_Animation/animation_viewer.py
+++ Lecture05_Animation/animation_viewer.py
@@ -8,7 +8,28 @@ character = load_image('ghost.png')
 size = 32
 # fill here
 frame = 0
+
+def move():
+    pass
+
+def walk():
+    pass
+
+def jump():
+    pass
+
+def attack():
+    pass
+
+def die():
+    pass
+
 while True:
+    move()
+    walk()
+    jump()
+    attack()
+    die()
 
 
 
